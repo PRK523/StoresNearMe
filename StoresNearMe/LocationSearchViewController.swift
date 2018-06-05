96379aaa8e9772bc664e9cd503960c37f03d2077
Pranoti Kulkarni <pranotikulkarni@Viveks-Air.fios-router.home>
2018-06-05 16:16:30 -0400

    first commit

---
+++ StoresNearMe/LocationSearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LocationSearchViewController.swift
+//  StoresNearMe
+//
+//  Created by Pranoti Kulkarni on 5/30/18.
+//  Copyright © 2018 Pranoti Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
